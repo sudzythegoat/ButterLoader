7e00866964fc87ab3b596377286e6925a54f6fa4
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-11-11 07:53:52 -0600

    Update butter.lua

--- src/butter.lua
+++ src/butter.lua
@@ -118,9 +118,17 @@ local function createUi()
     SprintButton.Parent = butterUi
     SprintButton.MouseButton1Click:Connect(s2s)
 
+    local ModButton = Instance.new("TextButton")
+    ModButton.Size = UDim2.new(0, 150, 0, 50)
+    ModButton.Position = UDim2.new(0.1, 0, 0.8, 0)
+    ModButton.BackgroundColor3 = BtnBg
+    ModButton.Text = "Shift 2 Sprint"
+    ModButton.Parent = butterUi
+    ModButton.MouseButton1Click:Connect(s2s)
+
     local EndButton = Instance.new("TextButton")
     EndButton.Size = UDim2.new(0, 150, 0, 50)
-    EndButton.Position = UDim2.new(0.1, 0, 0.95, 0)
+    EndButton.Position = UDim2.new(0.1, 0, 1.1, 0)
     EndButton.BackgroundColor3 = BtnBg
     EndButton.Text = "Minimize"
     EndButton.Parent = butterUi
