8cd81268f2c54ba3a6b8703327dee53cbda8d87d
Sudzy <167475761+sudzythegoat@users.noreply.github.com>
2024-11-10 22:43:32 -0600

    Update butter.lua

--- src/butter.lua
+++ src/butter.lua
@@ -1,3 +1,10 @@
+local function close()
+    game.CoreGui:FindFirstChild("butterUi"):Destroy()
+end
+
+if game.CoreGui:FindFirstChild("butterUi") then close()
+end 
+
 local butterUi = Instance.new("ScreenGui")
 butterUi.Name = "Butter Loader v1"
 butterUi.Parent = game.CoreGui
@@ -5,9 +12,17 @@ butterUi.Parent = game.CoreGui
 local BtnBg = Color3.fromRGB(224, 245, 66)
 
 local function createUi()
+    local Header = Instance.new("TextButton")
+    Header.Size = UDim2.new(0, 150, 0, 50)
+    Header.Position = UDim2.new(0.1, 0, 0.1, 0)
+    Header.BackgroundColor3 = BtnBg
+    Header.Text = "ButterLoader v1"
+    Header.Parent = butterUi
+    Header.MouseButton1Click:Connect(function() print("ButterLoader"))
+
     local EspButton = Instance.new("TextButton")
     EspButton.Size = UDim2.new(0, 150, 0, 50)
-    EspButton.Position = UDim2.new(0.1, 0, 0.1, 0)
+    EspButton.Position = UDim2.new(0.1, 0, 0.25, 0)
     EspButton.BackgroundColor3 = BtnBg
     EspButton.Text = "ESP"
     EspButton.Parent = butterUi
@@ -15,7 +30,7 @@ local function createUi()
     
     local FlyButton = Instance.new("TextButton")
     FlyButton.Size = UDim2.new(0, 150, 0, 50)
-    FlyButton.Position = UDim2.new(0.1, 0, 0.25, 0)
+    FlyButton.Position = UDim2.new(0.1, 0, 0.4, 0)
     FlyButton.BackgroundColor3 = BtnBg
     FlyButton.Text = "Fly"
     FlyButton.Parent = butterUi
@@ -23,18 +38,19 @@ local function createUi()
     
     local AimButton = Instance.new("TextButton")
     AimButton.Size = UDim2.new(0, 150, 0, 50)
-    AimButton.Position = UDim2.new(0.1, 0, 0.4, 0)
+    AimButton.Position = UDim2.new(0.1, 0, 0.65, 0)
     AimButton.BackgroundColor3 = BtnBg
-    AimButton.Text = "Fly"
+    AimButton.Text = "Aimbot"
     AimButton.Parent = butterUi
     AimButton.MouseButton1Click:Connect(aimbot)
 
     local EndButton = Instance.new("TextButton")
     EndButton.Size = UDim2.new(0, 150, 0, 50)
-    EndButton.Position = UDim2.new(0.1, 0, 0.65, 0)
+    EndButton.Position = UDim2.new(0.1, 0, 0.80, 0)
     EndButton.BackgroundColor3 = BtnBg
-    EndButton.Text = "Butter v1"
+    EndButton.Text = "Minimize"
     EndButton.Parent = butterUi
+    EndButton.MouseButton1Click:Connect(close)
 end
 
 createUi()
